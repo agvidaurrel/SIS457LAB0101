101c084a4e98b2077ab433a5dd40300a3cba5acb
alvaro vidaurre <cheft96.esparragos@gmail.com>
2022-04-04 22:14:14 -0400

    cambios

--- SIS457LAB0101/principal.cpp
+++ SIS457LAB0101/principal.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 int main() {
+	char v = 'v';
+	char s = 's';
 
-	cout << "hol ami nombre es alvaro soy de tupiza tengo 25 años" << endl;
-	system("pause");
+
+		while (v == 'v') {
+			cout << "hola mi nombre es Alvaro" << endl;
+			cout << "soy de Tupiza situado en el departamento de Potosi pero naci en la localidad de Oploca" << endl;
+			cout << "me mude a tupiza alos 5 años y vivi la mayor parte en tupiza" << endl;
+			cout << "los nombres de mis padres son LUCIA  y GROVER " << endl;
+			cout << "actualmente vivo en alquiler en la ciudad de  sucre para cursar la carrera de ING.SISTEMAS " << endl;
+			cout << "y me gustan los videojuegos!!! :D " << endl;
+			cout << "presione (v) para seguir \npara salir (s)" << endl;
+
+			cin >> v;
+		}
 	return 0;
- }
+}
